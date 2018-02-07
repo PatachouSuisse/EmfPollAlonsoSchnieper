e42cc98dadc01ae98854d13134ad094bce0912f6
schniepern <n.schnieper@gmx.ch>
2018-02-07 14:03:25 +0100

    Mise au propre du projet, ready for production

--- app/src/main/java/com/emfpoll/emfpoll/ctrl/Ctrl.java
+++
@@ -1,8 +0,0 @@
-package com.emfpoll.emfpoll.ctrl;
-
-/**
- * Created by alonsolopeza on 29.01.2018.
- */
-
-public class Ctrl {
-}
--- app/src/main/java/com/emfpoll/emfpoll/wrk/Wrk.java
+++
@@ -1,9 +0,0 @@
-package com.emfpoll.emfpoll.wrk;
-
-import android.provider.Settings;
-
-/**
- * Created by alonsolopeza on 29.01.2018.
- */
-
-public class Wrk {}
